afb58235607fc489ccffac3eeace29ba8ac38fa0
Boinga15 <realboinga@gmail.com>
2025-12-03 12:26:15 +0000

    Implemented GELU algorithm.

--- rust/src/one_bit_llm/parts.rs
+++ rust/src/one_bit_llm/parts.rs
@@ -1,4 +1,5 @@
 use nalgebra::*;
+use std::f64::consts::PI;
 
 
 pub trait Layer {
@@ -35,5 +36,32 @@ impl Layer for Dense {
     fn adjust_parameters(&mut self, learning_rate: f64) {
         self.weights -= &self.weight_gradients * learning_rate;
         self.biases -= &self.bias_gradients * learning_rate;
+    }
+}
+
+
+pub struct GELU {}
+
+impl GELU {
+    pub fn calculate(&mut self, input: DMatrix<f64>) -> DMatrix<f64> {
+        let mut cubed_input: DMatrix<f64> = input.clone();
+
+        for row in 0..cubed_input.shape().0 {
+            for column in 0..cubed_input.shape().1 {
+                cubed_input[(row, column)] = cubed_input[(row, column)].powi(3);
+            }
+        }
+
+        let mut inside_result: DMatrix<f64> = (&input + 0.044715 * cubed_input) * (2.0 / (PI));
+        let one_matrix: DMatrix<f64> = DMatrix::from_element(inside_result.shape().0, inside_result.shape().1, 1.0);
+
+        for row in 0..inside_result.shape().0 {
+            for column in 0..inside_result.shape().1 {
+                inside_result[(row, column)] = inside_result[(row, column)].tanh();
+            }
+        }
+
+
+        0.5 * &input * (one_matrix + inside_result)
     }
 }
