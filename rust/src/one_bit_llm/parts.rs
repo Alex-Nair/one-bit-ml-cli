b82ab341d889e1b518c1b5ab2b95b5284636c1f0
Boinga15 <realboinga@gmail.com>
2025-12-04 18:08:03 +0000

    Implemented base LayerNorm code.

--- rust/src/one_bit_llm/parts.rs
+++ rust/src/one_bit_llm/parts.rs
@@ -1,4 +1,5 @@
 use nalgebra::*;
+use rand::{self, Rng};
 use std::f64::consts::PI;
 
 
@@ -171,5 +172,141 @@ impl Layer for FFN {
     fn adjust_parameters(&mut self, learning_rate: f64) {
         self.first_layer.adjust_parameters(learning_rate);
         self.second_layer.adjust_parameters(learning_rate);
+    }
+}
+
+
+pub struct LayerNorm {
+    pub scaling_parameter: f64,
+    pub shifting_parameter: f64,
+
+    scaling_gradient: f64,
+    shifting_gradient: f64,
+
+    prev_mean: DMatrix<f64>,
+    prev_variance: DMatrix<f64>,
+
+    prev_inputs: DMatrix<f64>,
+    prev_normalized_inputs: DMatrix<f64>
+}
+
+impl LayerNorm {
+    pub fn new(range: f64) -> LayerNorm {
+        LayerNorm {
+            scaling_parameter: rand::thread_rng().gen_range((-1.0 * range)..range),
+            scaling_gradient: 0.0,
+
+            shifting_parameter: rand::thread_rng().gen_range((-1.0 * range)..range),
+            shifting_gradient: 0.0,
+
+            prev_mean: DMatrix::from_element(1, 1, 0.0),
+            prev_variance: DMatrix::from_element(1, 1, 0.0),
+
+            prev_inputs: DMatrix::from_element(1, 1, 0.0),
+            prev_normalized_inputs: DMatrix::from_element(1, 1, 0.0)
+        }
+    }
+
+    pub fn calculate(&mut self, input: DMatrix<f64>) -> DMatrix<f64> {
+        const EPSILON_CONSTANT: f64 = 0.00001;
+
+        self.prev_inputs = input.clone();
+
+        // Calculating mean and variance.
+        let mut means: DMatrix<f64> = DMatrix::from_element(1, input.shape().1, 0.0);
+        let mut variances: DMatrix<f64> = DMatrix::from_element(1, input.shape().1, 0.0);
+    
+        for col in 0..input.shape().1 {
+            for row in 0..input.shape().0 {
+                means[(0, col)] += input[(row, col)];
+            }
+
+            means[(0, col)] /= input.shape().0 as f64;
+        }
+
+        self.prev_mean = means.clone();
+
+        for col in 0..input.shape().1 {
+            for row in 0..input.shape().0 {
+                variances[(0, col)] += (input[(row, col)] - means[(0, col)]).powi(2);
+            }
+
+            variances[(0, col)] /= input.shape().0 as f64;
+        }
+
+        self.prev_variance = variances.clone();
+
+        // Normalizing Inputs
+        let mut normalized: DMatrix<f64> = DMatrix::from_element(input.shape().0, input.shape().1, 0.0);
+
+        for col in 0..input.shape().1 {
+            for row in 0..input.shape().0 {
+                normalized[(row, col)] = (input[(row, col)] - means[(0, col)]) / (variances[(0, col)] + EPSILON_CONSTANT).sqrt();
+            }
+        }
+
+        self.prev_normalized_inputs = normalized.clone();
+
+        let ones_matrix: DMatrix<f64> = DMatrix::from_element(input.shape().0, input.shape().1, 1.0);
+
+        self.scaling_parameter * normalized + (ones_matrix * self.shifting_parameter)
+    }
+}
+
+impl Layer for LayerNorm {
+    fn calculate_gradients(&mut self, previous_gradient: DMatrix<f64>) -> DMatrix<f64> {
+        const EPSILON_CONSTANT: f64 = 0.00001;
+        
+        for row in 0..previous_gradient.shape().0 {
+            for col in 0..previous_gradient.shape().1 {
+                self.scaling_gradient += self.prev_normalized_inputs[(row, col)] * previous_gradient[(row, col)];
+                self.shifting_gradient += previous_gradient[(row, col)];
+            }
+        }
+
+        let mut normalize_gradients: DMatrix<f64> = DMatrix::from_element(self.prev_normalized_inputs.shape().0, self.prev_normalized_inputs.shape().1, self.scaling_parameter);
+        
+        for row in 0..previous_gradient.shape().0 {
+            for col in 0..previous_gradient.shape().1 {
+                normalize_gradients[(row, col)] *= previous_gradient[(row, col)];
+            }
+        }
+        
+        // Calculating return gradients.
+        let mut input_gradients: DMatrix<f64> = DMatrix::from_element(self.prev_inputs.shape().0, self.prev_inputs.shape().1, 0.0);
+        let mut mean_gradients: DMatrix<f64> = DMatrix::from_element(1, self.prev_inputs.shape().1, 0.0);
+        let mut variance_gradients: DMatrix<f64> = DMatrix::from_element(1, self.prev_inputs.shape().1, 0.0);
+
+        for col in 0..variance_gradients.shape().1 {
+            for row in 0..normalize_gradients.shape().0 {
+                variance_gradients[(0, col)] += normalize_gradients[(row, col)] * (-1.0 * self.prev_variance[(0, col)].sqrt() * (self.prev_inputs[(row, col)] - self.prev_mean[(0, col)]) * (self.prev_variance[(0, col)] + EPSILON_CONSTANT).powf(-1.0 * (3.0 / 2.0)));
+            }
+        }
+
+        for col in 0..mean_gradients.shape().1 {
+            let mut column_total: f64 = 0.0;
+
+            for row in 0..normalize_gradients.shape().0 {
+                column_total += self.prev_inputs[(row, col)];
+                mean_gradients[(0, col)] += normalize_gradients[(row, col)] * (-1.0 / (self.prev_variance[(0, col)] + EPSILON_CONSTANT).sqrt());
+            }
+
+            mean_gradients[(0, col)] += variance_gradients[(0, col)] * ((2.0 * self.prev_mean[(0, col)]) - ((2.0 * column_total) / (normalize_gradients.shape().0 as f64)))
+        }
+
+        for col in 0..variance_gradients.shape().1 {
+            for row in 0..normalize_gradients.shape().0 {
+                input_gradients[(row, col)] += normalize_gradients[(row, col)] * (1.0 / (self.prev_variance[(0, col)] + EPSILON_CONSTANT).sqrt());
+                input_gradients[(row, col)] += variance_gradients[(0, col)] * ((2.0 * self.prev_inputs[(row, col)]) - (2.0 * self.prev_mean[(0, col)])) / normalize_gradients.shape().0 as f64;
+                input_gradients[(row, col)] += mean_gradients[(0, col)] * (1.0 / normalize_gradients.shape().0 as f64);
+            }
+        }
+
+        input_gradients
+    }
+    
+    fn adjust_parameters(&mut self, learning_rate: f64) {
+        self.scaling_parameter -= self.scaling_gradient * learning_rate;
+        self.shifting_parameter -= self.shifting_gradient * learning_rate;
     }
 }
